bb4b14ef33c523adfd0740658012bf2fcebe8556
vanshu-samrutwar <vanshu24june@gmail.com>
2025-08-30 08:40:50 +0530

    removed frontend package

--- frontend/src/hooks/useTranslation.ts
+++
@@ -1,7 +0,0 @@
-import { useLanguage } from '@/contexts/LanguageContext';
-import { translations, Translations } from '@/translations';
-
-export const useTranslation = (): Translations => {
-  const { language } = useLanguage();
-  return translations[language];
-};
